683093265db6b5d0d108f486a87e39552df6ed8c
MohamedMahmoudAli <mohamed238ma@gmail.com>
2022-09-24 14:18:42 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/consetants/consetants.dart';
+import 'package:to_do/screens/layout/navigationscreen.dart';
+import 'package:bloc/bloc.dart';
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeLayout(),
+    );
+  }
+}
---
+++ lib/models/userdata.dart
@@ -0,0 +1,6 @@
+class usersData {
+  final int Id;
+  final String name;
+  final String phone;
+  usersData({required this.Id, required this.name, required this.phone}) {}
+}
